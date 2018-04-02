fcd209e4409b99eb42baa01a45ea86ae00a9cb27
Revine Kim <skyho31@gmail.com>
2018-04-02 09:04:54 +0900

    1.0.0 complete

--- src/components/InnerImg.js
+++ src/components/InnerImg.js
@@ -3,7 +3,7 @@ import React from 'react';
 const InnerImg = (props) => {
     
   return (
-    <span className="horizontal-imgbox" style={props.style}>
+    <span className="horizontal-imgbox" style={props.style} onClick={props.onClick}>
       <img className={props.border && "border"} src={props.src} height={props.height ? props.height : 200} alt={props.text} width={props.width ? props.width : null} />
       <p className="img_description"> {props.text}</p>
     </span>
--- src/pages/Chapter/index.js
+++ src/pages/Chapter/index.js
@@ -1,89 +1,92 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Container, Col, Row} from 'reactstrap';
 
 /* Component */
 import SideTab from '../../components/SideTab';
 import Contents from './Contents';
 
-const Chapter = ({location, match}) => {
-  let options = [
-    {
-      route: true,
-      title: 'Basic',
-      items: [
-        {text: 'Concept & Process', href: `${match.url}`},
-      ]
-    },
-    {
-      route: true,
-      title: 'Quick Start',
-      items: [
-        {text: '1. Collect External Data', href: `${match.url}/quickstart/step1`},
-        {text: '2. Parse Data', href: `${match.url}/quickstart/step2`},
-        {text: '3. Send Data to Processor', href: `${match.url}/quickstart/step3`},
-        {text: '4. Aggregate Data', href: `${match.url}/quickstart/step4`}
-      ]
-    },
-    // {
-    //   route: true,
-    //   title: 'Node',
-    //   items: [
-    //     {text: 'Types of Nodes', href: `${match.url}/node/typeofnodes`},
-    //     {text: 'Basic Nodes', href: `${match.url}/node/basicnode`},
-    //     {text: 'Config Nodes', href: `${match.url}/node/confignode`},
-    //     {text: 'ADP Nodes', href: `${match.url}/node/adpnode`},
-    //     {text: 'Installing Add-on Nodes', href: `${match.url}/node/installingaddonnode`}
-    //   ]
-    // },
-    // {
-    //   route: true,
-    //   title: 'Flow',
-    //   items: [
-    //     {text: 'Flow', href: `${match.url}/flow/simpleflow`},
-    //     {text: 'Sub Flow', href: `${match.url}/flow/subflow`},
-    //     {text: 'Copy & Paste Flow', href: `${match.url}/flow/copypasteflow`},
-    //     {text: 'Import & Export Flow', href: `${match.url}/flow/importexportflow`}
-    //   ]
-    // },
-    // {
-    //   route: true,
-    //   title: 'Processor',
-    //   items: [
-    //     {text: 'Processor Server Flow', href: `${match.url}/flow/subflow`},
-    //     {text: 'RESTful Service', href: `${match.url}/flow/importexportflow`},
-    //     {text: 'Websocket Service', href: `${match.url}/flow/copypasteflow`},
-    //     {text: 'DB Schema', href: `${match.url}/flow/importexportflow`},
-    //   ]
-    // },
-    // {
-    //   title: 'Advanced Guide',
-    //   items: [
-    //     {text: 'Confluence page', href: `https://n3n-us.atlassian.net/wiki/spaces/teamdev/pages/84061742/ADP+Web+Service`},
-    //     {text: 'Dataengine PDF manual', href: '#'}
-    //   ]
-    // }
-    {
-      title: 'Advanced Guide',
-      items: [
-        {text: 'Editor/Runtime', href: `https://n3n-us.atlassian.net/wiki/spaces/teamdev/pages/84061742/ADP+Web+Service`},
-        {text: 'Processor', href: 'https://n3n-us.atlassian.net/wiki/spaces/teamdev/pages/440565764/Processor+Server+Guide'},
-        {text: 'Dataengine PDF manual', href: '#'}
-      ]
-    }
-  ];
+/* document */
+import pdf from '../../libs/WIZEYE_V2.0_ADMIN_USER_GUIDE_v1.5_Data관리.pdf';
 
-  return (
-    <Container>
-      <Row>
-        <Col md="9">
-          <Contents />
-        </Col>
-        <Col>
-          <SideTab options={options} match={match}/>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+class Chapter extends Component {
+  render() {
+    let options = [
+      {
+        route: true,
+        title: 'Basic',
+        items: [{text: 'Concept & Process', href: `${this.props.match.url}`}]
+      },
+      {
+        route: true,
+        title: 'Quick Start',
+        items: [
+          {text: '1. Collect External Data', href: `${this.props.match.url}/quickstart/step1`},
+          {text: '2. Parse Data', href: `${this.props.match.url}/quickstart/step2`},
+          {text: '3. Send Data to Processor', href: `${this.props.match.url}/quickstart/step3`},
+          {text: '4. Aggregate Data', href: `${this.props.match.url}/quickstart/step4`}
+        ]
+      },
+      // {
+      //   route: true,
+      //   title: 'Node',
+      //   items: [
+      //     {text: 'Types of Nodes', href: `${match.url}/node/typeofnodes`},
+      //     {text: 'Basic Nodes', href: `${match.url}/node/basicnode`},
+      //     {text: 'Config Nodes', href: `${match.url}/node/confignode`},
+      //     {text: 'ADP Nodes', href: `${match.url}/node/adpnode`},
+      //     {text: 'Installing Add-on Nodes', href: `${match.url}/node/installingaddonnode`}
+      //   ]
+      // },
+      // {
+      //   route: true,
+      //   title: 'Flow',
+      //   items: [
+      //     {text: 'Flow', href: `${match.url}/flow/simpleflow`},
+      //     {text: 'Sub Flow', href: `${match.url}/flow/subflow`},
+      //     {text: 'Copy & Paste Flow', href: `${match.url}/flow/copypasteflow`},
+      //     {text: 'Import & Export Flow', href: `${match.url}/flow/importexportflow`}
+      //   ]
+      // },
+      // {
+      //   route: true,
+      //   title: 'Processor',
+      //   items: [
+      //     {text: 'Processor Server Flow', href: `${match.url}/flow/subflow`},
+      //     {text: 'RESTful Service', href: `${match.url}/flow/importexportflow`},
+      //     {text: 'Websocket Service', href: `${match.url}/flow/copypasteflow`},
+      //     {text: 'DB Schema', href: `${match.url}/flow/importexportflow`},
+      //   ]
+      // },
+      // {
+      //   title: 'Advanced Guide',
+      //   items: [
+      //     {text: 'Confluence page', href: `https://n3n-us.atlassian.net/wiki/spaces/teamdev/pages/84061742/ADP+Web+Service`},
+      //     {text: 'Dataengine PDF manual', href: '#'}
+      //   ]
+      // }
+      {
+        title: 'Advanced Guide',
+        items: [
+          {text: 'Editor/Runtime', href: `https://n3n-us.atlassian.net/wiki/spaces/teamdev/pages/84061742/ADP+Web+Service`},
+          {text: 'Processor', href: 'https://n3n-us.atlassian.net/wiki/spaces/teamdev/pages/440565764/Processor+Server+Guide'},
+          {text: 'Dataengine PDF manual', href: pdf}
+        ]
+      }
+    ];
+
+    return (
+      <Container>
+        <Row>
+          <Col md="9">
+            <Contents />
+          </Col>
+          <Col>
+            <SideTab options={options} match={this.props.match} />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
 
 export default Chapter;
